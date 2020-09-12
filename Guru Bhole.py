203bf86ca1fab2592ff58057f48095ef84daf57e
h510-cell <58731327+h510-cell@users.noreply.github.com>
2020-09-12 14:12:17 +0530

    Add files via upload

---
+++ Guru Bhole.py
@@ -0,0 +1,11 @@
+class atm():
+    def _init_(self,atm_card_number,pin_number):
+        self.atm_card_number=atm_card_number
+        self.pin_number=pin_number
+
+    def CashWithdrawl(self):
+        print("CashWithdrawl")
+
+    def BalanceEnquiry(self):
+        print("BalanceEnquiry")
+                 
